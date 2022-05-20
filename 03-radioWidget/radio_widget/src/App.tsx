0d3ca5461fc00c6360bb010b27d42706fd653b20
urreita9 <francisco.urrea9@gmail.com>
2022-05-20 18:02:51 -0300

    conditional rendering of RadioContainer

--- 03-radioWidget/radio_widget/src/App.tsx
+++ 03-radioWidget/radio_widget/src/App.tsx
@@ -1,3 +1,4 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 
 import './App.module.css';
@@ -5,9 +6,10 @@ import { AuthContainer } from './components/AuthContainer/AuthContainer';
 import { RadioContainer } from './components/RadioContainer/RadioContainer';
 
 function App() {
+	const { user } = useAuth0();
 	return (
 		<div className='App'>
-			<RadioContainer />
+			{user && <RadioContainer />}
 
 			<AuthContainer />
 		</div>
