aae847da7073e92f26aa03dfcfd5713df0f05f8a
urreita9 <francisco.urrea9@gmail.com>
2022-05-20 17:36:10 -0300

    auth0 Provider =>React

--- 03-radioWidget/radio_widget/src/index.tsx
+++ 03-radioWidget/radio_widget/src/index.tsx
@@ -4,14 +4,20 @@ import './index.module.css';
 import App from './App';
 import { store } from './redux/store/store';
 import { Provider } from 'react-redux';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<Auth0Provider
+			domain='dev-efi5m3ft.us.auth0.com'
+			clientId='6UPUU5CtTNh8jtP1ZhLnOqIJpWUstdGd'
+		>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</Auth0Provider>
 	</React.StrictMode>
 );
