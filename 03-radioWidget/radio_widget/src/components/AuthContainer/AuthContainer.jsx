d1873e5d6c5612da377f394377b6cf245b9a3bd5
urreita9 <francisco.urrea9@gmail.com>
2022-05-21 16:32:23 -0300

    Token appears after login so that requests can be made through postman

--- 03-radioWidget/radio_widget/src/components/AuthContainer/AuthContainer.jsx
+++ 03-radioWidget/radio_widget/src/components/AuthContainer/AuthContainer.jsx
@@ -1,8 +1,25 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useState } from 'react';
 import styles from './AuthContainer.module.css';
 
 export const AuthContainer = () => {
-	const { loginWithPopup, logout, user, isAuthenticated } = useAuth0();
+	const {
+		loginWithPopup,
+		logout,
+		user,
+		isAuthenticated,
+		getAccessTokenSilently,
+	} = useAuth0();
+	const [token, setToken] = useState('');
+	useEffect(() => {
+		if (isAuthenticated) {
+			getToken();
+		}
+	}, [isAuthenticated]);
+
+	const getToken = async () => {
+		setToken(await getAccessTokenSilently());
+	};
 	return (
 		<>
 			<div className={styles.container}>
@@ -21,6 +38,11 @@ export const AuthContainer = () => {
 						<pre style={{ color: 'white' }}>
 							{JSON.stringify(user, null, 2)}
 						</pre>
+						<h3>Token</h3>
+						<h4>Copy & paste in postman (Headers)</h4>
+						<h5 style={{ wordBreak: 'break-all' }}>
+							"authorization" // "Bearer {token}"
+						</h5>
 					</>
 				) : (
 					<h5 className={styles.loggedOut}>Logged out</h5>
