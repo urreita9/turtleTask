85529e415bddc93d893fe864399f9e28c3960ad9
urreita9 <francisco.urrea9@gmail.com>
2022-05-21 17:37:56 -0300

    postman doc

--- 04-ApiServer/index.js
+++ 04-ApiServer/index.js
@@ -3,15 +3,9 @@ require('dotenv').config();
 const dbConnection = require('./database/config');
 const cors = require('cors');
 const app = express();
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
 
 const radioRouter = require('./routes/radioRoutes');
-const { swaggerOptions } = require('./swagger/options');
 
-const specs = swaggerJsDoc(swaggerOptions);
-//SWAGGER
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 //Database
 dbConnection();
 
